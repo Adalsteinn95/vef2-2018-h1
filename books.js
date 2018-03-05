93775da9219bb8958be7075997e758fa10092efd
Fannar <fannargauti@gmail.com>
2018-03-05 12:59:41 +0000

    validates post requests

---
+++ books.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
+const router = express.Router();
+
+/*
+GET skilar síðu af bókum
+*/
+router.get('/', (req, res) => {
+  // do stuff
+});
+
+/*
+POST býr til nýja bók ef hún er gild og skilar
+*/
+router.post(
+  '/',
+  check('title')
+    .isEmpty()
+    .withMessage('Titill bókar má ekki vera tómur'),
+  check('ISBN13')
+    .isISBN({ version: 13 })
+    .withMessage('ISBN 13 er ekki á réttu formi'),
+  check('ISBN10')
+    .isISBN({ version: 10 })
+    .withMessage('ISBN 10 er ekki á réttu formi'),
+  check('pages')
+    .isInt({ min: 0 })
+    .withMessage('Blaðsíðufjöldi verður að vera tala, stærri en 0'),
+  check('language')
+    .isLength({ min: 2, max: 2 })
+    .withMessage('Tungumál verður að vera tveggja stafa strengur'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // do stuff
+    }
+    // do other stuff
+  },
+);
+
+/*
+GET skilar síðu af bókum sem uppfylla leitarskilyrði, sjá að neðan
+*/
+router.get('/books?search=query', (req, res) => {
+  // do stuff
+});
+
+/*
+GET skilar stakri bók
+*/
+router.get('/:id', (req, res) => {
+  // do stuff
+});
+
+/*
+PATCH uppfærir bók
+*/
+router.post('/:id', (req, res) => {
+  // do stuff
+});
---
+++ users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
+const router = express.Router();
+
+/*
+GET skilar síðu (sjá að neðan) af notendum
+lykilorðs hash skal ekki vera sýnilegt
+*/
+router.get('/', (req, res) => {
+  // do stuff
+});
+
+/*
+GET skilar stökum notanda ef til
+Lykilorðs hash skal ekki vera sýnilegt
+*/
+router.get('/:id', (req, res) => {
+  // do stuff
+});
+
+/*
+GET skilar innskráðum notanda (þ.e.a.s. þér)
+*/
+router.get('/me', (req, res) => {
+  // do stuff
+});
+
+/*
+PATCH uppfærir sendar upplýsingar um notanda fyrir utan notendanafn,
+þ.e.a.s. nafn eða lykilorð, ef þau eru gild
+*/
+router.patch(
+  '/me',
+  check('password')
+    .isLength({ min: 6 })
+    .withMessage('Lykilorð verður að vera amk 6 stafir'),
+  check('name')
+    .isEmpty()
+    .withMessage('Nafn má ekki vera tómt'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // do stuff
+    }
+    // do other stuff
+  },
+);
+
+/*
+POST setur eða uppfærir mynd fyrir notanda í gegnum Cloudinary og skilar slóð
+*/
+router.get('/me/profile', (req, res) => {
+  // do stuff
+});
+
+/*
+GET skilar síðu af lesnum bókum notanda
+*/
+router.get('/users/:id/read', (req, res) => {
+  // do stuff
+});
+
+/*
+GET skilar síðu af lesnum bókum innskráðs notanda
+*/
+router.get('/users/me/read', (req, res) => {
+  // do stuff
+});
+
+/*
+POST býr til nýjan lestur á bók og skilar
+*/
+router.post(
+  '/users/me/read',
+  check('rating')
+    .isInt({ min: 1, max: 5 })
+    .withMessage('Einkunn verður að vera tala á bilinu 1-5'),
+  (req, res) => {
+    // do stuff
+  },
+);
+
+/*
+DELETE eyðir lestri bókar fyrir innskráðan notanda
+*/
+router.delete('/users/me/read/:id', (req, res) => {
+  // do stuff
+});
