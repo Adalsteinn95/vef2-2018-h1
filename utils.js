3e30bc0ea31d3d927c3db7170322c671c7c65d96
adalsteinn95 <adalsteinn95@gmail.com>
2018-03-12 11:56:33 +0000

    error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,11 @@
+
+/**
+ * higher order error function to catch errors
+ *
+ * @param {function} fn - function
+ *
+ * @returns {function} wrapped with error handling
+ */
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
